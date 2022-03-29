4e898817cf50426ce7625af816a658ecefc26180
Ryan Bender <ryan_bender@ymail.com>
2022-03-29 14:17:46 -0500

    hiding rich text edit

--- react-app/src/components/Home/home.js
+++ react-app/src/components/Home/home.js
@@ -48,7 +48,7 @@ const Home = () => {
                         })}
                 </div>
             </div>
-            <RichText />
+            {/* <RichText /> */}
         </div>
         </>
     )
